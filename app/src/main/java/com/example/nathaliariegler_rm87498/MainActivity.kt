685ef6d199ca4230a6850dcbac793d2556d357e7
Nathalia Schwertner <nathalia_13@outlook.com>
2023-05-24 01:49:54 -0300

    first commit

---
+++ app/src/main/java/com/example/nathaliariegler_rm87498/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.nathaliariegler_rm87498
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.mainfragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+}
